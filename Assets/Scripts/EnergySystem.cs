6f7ac33fde0edd03f13181aff3bc8cef1f702693
seungdo1234 <86179438+seungdo1234@users.noreply.github.com>
2024-05-14 09:27:44 +0900

    Init

---
+++ Assets/Scripts/EnergySystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class EnergySystem : MonoBehaviour
+{
+    public event Action<float> OnEnergyChanged;
+    public float MaxFuel { get; private set; } = 10f;
+    [field:SerializeField] public float Fuel { get; private set; } = 10f;
+    
+    public bool UseEnergy(float amount)
+    {
+        if (Fuel < amount) return false;
+        // TODO : 에너지를 사용하는 로직을 적용
+        Fuel -= amount;
+        OnEnergyChanged?.Invoke(Fuel);
+        
+        return true;
+    }
+}
